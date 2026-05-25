e6f02f3595ff2718976153aa227a9d22c233e82c
randomwheel <mubeenahmedzex@gmail.com>
2026-05-25 23:48:36 +0500

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -61,6 +61,7 @@ const Footer = {
               <li><a href="/#wheel-types">All Wheel Types</a></li>
               <li><a href="/about">About</a></li>
               <li><a href="/contact">Contact</a></li>
+              <li><a href="/blog">Blog</a></li>
             </ul>
           </div>
         </div>
